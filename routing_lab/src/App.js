d23fbd9d160b049c6ffaa72953ec7cc7793bbedd
Ohidurr <Ohidurrahman96@gmail.com>
2020-02-26 21:17:06 -0500

    create singleDog and added npm i axios

--- routing_lab/src/App.js
+++ routing_lab/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import SingleDog from './components/dogs/SingleDog'
 import { Route } from 'react-router-dom'
 
 
@@ -7,6 +8,8 @@ function App() {
   return (
     <div className="App">
       This is a test
+    <Route path={"/dog/random"} component = {SingleDog} />
+      <SingleDog/>
     </div>
   );
 }
--- routing_lab/src/components/dogs/SingleDog.js
+++ routing_lab/src/components/dogs/SingleDog.js
@@ -1,2 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
+import Dog from './Dog'
 
+class SingleDog extends Component{
+    state= {
+        DogImg: null
+    }
+
+    getDogPic = async () => {
+        const url = 'https://dog.ceo/api/breeds/image/random'
+        try{
+            let res = await axios.get(url)
+            debugger
+            this.setState({DogImg: res.data.message})
+
+            
+        }catch(error){
+            console.log(error);
+            
+        }
+    }
+
+    handleButton = (e) => {
+        return(
+            <Dog img={Dog}/>
+        )
+    }
+    render(){
+        this.handle
+        return(
+            <div>
+            {Dog}
+                SingleDog Div 
+                <button onClick={this.getDogPic}>Get random dog</button>
+            </div>
+        )
+    }
+}
+
+export default SingleDog
